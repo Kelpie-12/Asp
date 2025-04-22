89fee21c30c5ea41504830fe4421f416cd84fd0e
roman <roman121998yarkov@gmail.com>
2025-04-22 23:16:30 +0400

    Add project files.

---
+++ Restoran/Pages/Adress.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Restoran.Pages
+{
+    public class AdressModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
