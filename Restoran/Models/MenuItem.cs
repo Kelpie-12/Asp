4cf43055b9a359a43bf7054bceef4775fa3f8342
roman <roman121998yarkov@gmail.com>
2025-04-30 12:18:13 +0400

    hw

--- Restoran/Models/MenuItem.cs
+++ Restoran/Models/MenuItem.cs
@@ -3,35 +3,9 @@ namespace Restoran.Models
 {
     public class MenuItem
     {
-		private int id;
-
-		public int Id
-		{
-			get { return id; }
-			set { id = value; }
-		}
-		private string name;
-
-		public string Name
-		{
-			get { return name; }
-			set { name = value; }
-		}
-		private int price;
-
-		public int Price
-		{
-			get { return price; }
-			set { price = value; }
-		}
-		private string desc;
-
-		public string Desc
-		{
-			get { return desc; }
-			set { desc = value; }
-		}
-
-
-	}
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int Price { get; set; }
+        public string? Desc { get; set; }
+    }
 }
